9a054b78ffede7d377a91fe9ea248908d5e097ac
Shivraj <shivkolekar01@gmail.com>
2024-06-13 00:44:05 +0530

    updated js code

--- main.js
+++ main.js
@@ -1,11 +1,8 @@
-import "./style.css";
-import javascriptLogo from "./javascript.svg";
-import viteLogo from "/vite.svg";
-import { setupCounter } from "./counter.js";
-import Typed from 'typed.js';
+import './style.css'
 
+import Typed from 'typed.js'
 
-document.querySelector("#app").innerHTML = `
+document.querySelector('#app').innerHTML = `
 
 <div id="body" class="px-4 sm:px-10 lg:px-20 bg-scroll">
 
@@ -306,60 +303,62 @@ document.querySelector("#app").innerHTML = `
 
 
 
-`;
-var themeToggleDarkIcon = document.getElementById('theme-toggle-dark-icon');
-var themeToggleLightIcon = document.getElementById('theme-toggle-light-icon');
+`
+var themeToggleDarkIcon = document.getElementById('theme-toggle-dark-icon')
+var themeToggleLightIcon = document.getElementById('theme-toggle-light-icon')
 
 // Change the icons inside the button based on previous settings
-if (localStorage.getItem('color-theme') === 'dark' || (!('color-theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
-    themeToggleLightIcon.classList.remove('hidden');
+if (
+  localStorage.getItem('color-theme') === 'dark' ||
+  (!('color-theme' in localStorage) &&
+    window.matchMedia('(prefers-color-scheme: dark)').matches)
+) {
+  themeToggleLightIcon.classList.remove('hidden')
 } else {
-    themeToggleDarkIcon.classList.remove('hidden');
+  themeToggleDarkIcon.classList.remove('hidden')
 }
 
-var themeToggleBtn = document.getElementById('theme-toggle');
+var themeToggleBtn = document.getElementById('theme-toggle')
 
-themeToggleBtn.addEventListener('click', function() {
+themeToggleBtn.addEventListener('click', function () {
+  // toggle icons inside button
+  themeToggleDarkIcon.classList.toggle('hidden')
+  themeToggleLightIcon.classList.toggle('hidden')
 
-    // toggle icons inside button
-    themeToggleDarkIcon.classList.toggle('hidden');
-    themeToggleLightIcon.classList.toggle('hidden');
-
-    // if set via local storage previously
-    if (localStorage.getItem('color-theme')) {
-        if (localStorage.getItem('color-theme') === 'light') {
-            document.documentElement.classList.add('dark');
-            localStorage.setItem('color-theme', 'dark');
-        } else {
-            document.documentElement.classList.remove('dark');
-            localStorage.setItem('color-theme', 'light');
-        }
+  // if set via local storage previously
+  if (localStorage.getItem('color-theme')) {
+    if (localStorage.getItem('color-theme') === 'light') {
+      document.documentElement.classList.add('dark')
+      localStorage.setItem('color-theme', 'dark')
+    } else {
+      document.documentElement.classList.remove('dark')
+      localStorage.setItem('color-theme', 'light')
+    }
 
     // if NOT set via local storage previously
-    } else {
-        if (document.documentElement.classList.contains('dark')) {
-            document.documentElement.classList.remove('dark');
-            localStorage.setItem('color-theme', 'light');
-        } else {
-            document.documentElement.classList.add('dark');
-            localStorage.setItem('color-theme', 'dark');
-        }
-    }
-    
-});
-var typed = new Typed('#typed', {
-
-  stringsElement:"#typed-strings",
- backSpeed:40,
-  typeSpeed: 60
-});
-
-
-document.getElementById('toggle-menu-button').addEventListener('click', function() {
-  var menu = document.getElementById('menu');
-  if (menu.style.top === '0px') {
-    menu.style.top = '-100%';
   } else {
-    menu.style.top = '0px';
+    if (document.documentElement.classList.contains('dark')) {
+      document.documentElement.classList.remove('dark')
+      localStorage.setItem('color-theme', 'light')
+    } else {
+      document.documentElement.classList.add('dark')
+      localStorage.setItem('color-theme', 'dark')
+    }
   }
-});
+})
+var typed = new Typed('#typed', {
+  stringsElement: '#typed-strings',
+  backSpeed: 40,
+  typeSpeed: 60
+})
+
+document
+  .getElementById('toggle-menu-button')
+  .addEventListener('click', function () {
+    var menu = document.getElementById('menu')
+    if (menu.style.top === '0px') {
+      menu.style.top = '-100%'
+    } else {
+      menu.style.top = '0px'
+    }
+  })
